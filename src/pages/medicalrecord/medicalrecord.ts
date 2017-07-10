b5ab419a02ad6026d45c7bea87cde776dd59bd2d
Pliable Pixels <pliablepixels@gmail.com>
2017-07-10 15:01:43 -0400

    took out crop - bad interface

--- src/pages/medicalrecord/medicalrecord.ts
+++ src/pages/medicalrecord/medicalrecord.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ModalController, IonicPage, NavParams, AlertController, NavController, ActionSheetController, Platform, LoadingController, Loading } from 'ionic-angular';
 import { Camera } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
@@ -16,7 +16,7 @@ import { AndroidPermissions } from '@ionic-native/android-permissions';
 @Component({
   selector: 'page-medicalrecord',
   templateUrl: 'medicalrecord.html',
-  
+
 })
 export class MedicalRecordPage {
   selectedCategory: any;
@@ -24,45 +24,44 @@ export class MedicalRecordPage {
 
   lastImage: string = null;
   loading: Loading;
-  viewMode:string;
+  viewMode: string;
   term: string = '';
-  searchEnabled:boolean = false;
-  cardSettings:boolean = false;
-  categories:any;
+  searchEnabled: boolean = false;
+  cardSettings: boolean = false;
+  categories: any;
 
-  constructor( public modal: ModalController,  public nav: NavController,  public navParams: NavParams,  public alertCtrl: AlertController,  public actionSheetCtrl: ActionSheetController, public platform: Platform, public loadingCtrl: LoadingController, public socialSharing: SocialSharing, public imageViewerCtrl: ImageViewerController, public commonUtils:CommonUtilsProvider, public camera:Camera, public file:File, public filePath:FilePath, public crop:Crop, public androidPermissions: AndroidPermissions) {
+  constructor(public modal: ModalController, public nav: NavController, public navParams: NavParams, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, public platform: Platform, public loadingCtrl: LoadingController, public socialSharing: SocialSharing, public imageViewerCtrl: ImageViewerController, public commonUtils: CommonUtilsProvider, public camera: Camera, public file: File, public filePath: FilePath, public crop: Crop, public androidPermissions: AndroidPermissions) {
 
     console.log("medicalrecord page constructor");
     this.selectedCategory = this.navParams.get('category');
     this.categories = this.navParams.get('root');
 
-    for (let i=0; i < this.categories.length; i++)
-    {
-      console.log ("**** CATEGORY "+this.categories[i].title);
+    for (let i = 0; i < this.categories.length; i++) {
+      console.log("**** CATEGORY " + this.categories[i].title);
     }
-  
+
     this.viewMode = 'cards';
-     }
+  }
 
-isMultiplePhotos(item) {
-  return item.photo.length > 1;
-}
+  isMultiplePhotos(item) {
+    return item.photo.length > 1;
+  }
 
-toggleSettingsCards() {
-  this.cardSettings = !this.cardSettings;
-}
+  toggleSettingsCards() {
+    this.cardSettings = !this.cardSettings;
+  }
 
- toggleSearch():void {
-   this.searchEnabled = !this.searchEnabled;
- }
- searchFn(ev: any) {
-   //console.log ("SEARCHFN");
+  toggleSearch(): void {
+    this.searchEnabled = !this.searchEnabled;
+  }
+  searchFn(ev: any) {
+    //console.log ("SEARCHFN");
     this.term = ev.target.value;
   }
 
-  presentModal (item) {
+  presentModal(item) {
     //console.log ("SENDING ITEM="+JSON.stringify(item));
-    let im = this.modal.create("ItemViewPage",{"item":item});
+    let im = this.modal.create("ItemViewPage", { "item": item });
     im.present();
 
     /*let modal = this.modal.create("GalleryModal", {
@@ -74,8 +73,8 @@ modal.present();*/
   }
 
   // not currenty used
-  presentImage (myImage) {
-    console.log ("PresentImage with "+myImage);
+  presentImage(myImage) {
+    console.log("PresentImage with " + myImage);
     const imageViewer = this.imageViewerCtrl.create(myImage);
     imageViewer.present();
   }
@@ -111,15 +110,13 @@ modal.present();*/
           text: 'Dummy',
           handler: () => {
             //this.takePicture(this.camera.PictureSourceType.CAMERA);
-            if (!item)
-            {
-                this.selectedCategory.record.addItem({ notes: '', photo: ['assets/images/doctor.png'] });
+            if (!item) {
+              this.selectedCategory.record.addItem({ notes: '', photo: ['assets/images/doctor.png'] });
             }
-            else
-            {
-                this.selectedCategory.record.addPhotoToItem('assets/images/doctor.png',item);
+            else {
+              this.selectedCategory.record.addPhotoToItem('assets/images/doctor.png', item);
             }
-            
+
           }
         }
       );
@@ -129,11 +126,11 @@ modal.present();*/
     actionSheet.present();
   }
 
-  
+
 
   // Always get the accurate path to your apps folder
   public getFullPath(img) {
-    let path =  this.commonUtils.getFullPath(img);
+    let path = this.commonUtils.getFullPath(img);
     // WKWebView specific code - doesn't work 
     /*
     if (this.platform.is('ios'))
@@ -149,50 +146,49 @@ modal.present();*/
   }
 
   addItem() {
-       if (this.platform.is('core')) {
-       this.presentActionSheet();
-       return;
+    if (this.platform.is('core')) {
+      this.presentActionSheet();
+      return;
     }
 
     this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
-  success => { 
-    console.log('Permission granted,taking picture');
-    this.presentActionSheet();
-  },
-  err => {
-    this.androidPermissions.requestPermissions(this.androidPermissions.PERMISSION.CAMERA).then (success => {this.presentActionSheet();}, err => {this.commonUtils.presentToast("Please grant camera permission","error");});
+      success => {
+        console.log('Permission granted,taking picture');
+        this.presentActionSheet();
+      },
+      err => {
+        this.androidPermissions.requestPermissions(this.androidPermissions.PERMISSION.CAMERA).then(success => { this.presentActionSheet(); }, err => { this.commonUtils.presentToast("Please grant camera permission", "error"); });
+      }
+    );
+
+
   }
-);
-  
 
+  addPhotoToItem(item): void {
+    //this.selectedCategory.record.addPhotoToItem(photo,item)
+    this.presentActionSheet(item);
   }
- 
- addPhotoToItem(item): void {
-   //this.selectedCategory.record.addPhotoToItem(photo,item)
-   this.presentActionSheet(item);
- }
 
 
-  removeItem(item, removeFile=true): void {
-    console.log ("Calling model remove");
-   this.selectedCategory.record.removeItem(item, removeFile)
-   .then (success => {
-     //this.commonUtils.presentToast('success deleting');
-   }, 
-   error => {
-     //this.commonUtils.presentToast('error deleting image','error');
-   });
+  removeItem(item, removeFile = true): void {
+    console.log("Calling model remove");
+    this.selectedCategory.record.removeItem(item, removeFile)
+      .then(success => {
+        //this.commonUtils.presentToast('success deleting');
+      },
+      error => {
+        //this.commonUtils.presentToast('error deleting image','error');
+      });
   }
 
   getCategory(val) {
-    let category:any = undefined;
-    console.log ("Searching for category="+val);
-    
-    for (let i=0; i < this.categories.length; i++) {
-      console.log("comparing to"+this.categories[i].title);
-      if (this.categories[i].title == val)
-      {
-        console.log ("FOUND");
+    let category: any = undefined;
+    console.log("Searching for category=" + val);
+
+    for (let i = 0; i < this.categories.length; i++) {
+      console.log("comparing to" + this.categories[i].title);
+      if (this.categories[i].title == val) {
+        console.log("FOUND");
         category = this.categories[i];
         break;
       }
@@ -212,12 +208,12 @@ modal.present();*/
         {
           text: 'Save',
           handler: data => {
-            console.log ("GOT "+data);
+            console.log("GOT " + data);
             let cat = this.getCategory(data);
             if (cat) {
-              console.log ("MOVING to "+cat);
+              console.log("MOVING to " + cat);
 
-              cat.record.addItem({notes:item.notes, photo:item.photo, date:item.date});
+              cat.record.addItem({ notes: item.notes, photo: item.photo, date: item.date });
               this.removeItem(item, false);
 
             }
@@ -226,17 +222,15 @@ modal.present();*/
         }
       ]
     };
-    for (let i=0; i < this.categories.length; i++)
-    {
-      if (this.selectedCategory.title != this.categories[i].title )
-      {
-          options.inputs.push ({type:'radio',label:this.categories[i].title, value: this.categories[i].title});
+    for (let i = 0; i < this.categories.length; i++) {
+      if (this.selectedCategory.title != this.categories[i].title) {
+        options.inputs.push({ type: 'radio', label: this.categories[i].title, value: this.categories[i].title });
       }
-      
+
     }
     let prompt = this.alertCtrl.create(options);
-    
-    
+
+
     prompt.present();
   }
 
@@ -287,8 +281,7 @@ modal.present();*/
       chooserTitle: 'Share via...'
     };
 
-    for (let i=0; i< item.photo.length; i++)
-    {
+    for (let i = 0; i < item.photo.length; i++) {
       options.files.push(this.getFullPath(item.photo[i]));
     }
 
@@ -302,102 +295,70 @@ modal.present();*/
 
 
 
-   // Copy the image to a local folder
-   copyFileToLocalDir(namePath, currentName, newFileName): Promise <any> {
-    console.log ("inside copyFileToLocal Dir *** persistent "+this.file.dataDirectory);
-    return this.file.copyFile(namePath, currentName,  this.file.dataDirectory, newFileName);
-    
+  // Copy the image to a local folder
+  copyFileToLocalDir(namePath, currentName, newFileName): Promise<any> {
+    console.log("inside copyFileToLocal Dir *** persistent " + this.file.dataDirectory);
+    return this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName);
+
   }
 
- takePicture(sourceType, item?) {
+  takePicture(sourceType, item?) {
     // if item provided then we add a photo to it
     var options = {
       quality: 70,
       sourceType: sourceType,
       saveToPhotoAlbum: false,
       correctOrientation: true,
-      allowEdit:false
+      allowEdit: false
     };
 
     // Get the data of an image
     let newFileName = this.commonUtils.createFileName();
     this.camera.getPicture(options).then((imagePath) => {
       // Special handling for Android library
-      console.log ("---->getPicture returned "+imagePath);
-      this.crop.crop(imagePath).then ((croppedPath) => {
-        // REMOVE
-        //croppedPath = imagePath;
-        if (croppedPath.indexOf('?') > -1) {
-          croppedPath = croppedPath.slice(0,croppedPath.indexOf('?'));
+      console.log("---->getPicture returned " + imagePath);
+ 
+      this.commonUtils.presentLoader('saving...');
+      this.savePhoto(newFileName, imagePath, sourceType).then(success => {
+        this.commonUtils.removerLoader();
+        if (!item) {
+          this.selectedCategory.record.addItem({ notes: '', photo: [newFileName] });
         }
-         console.log ("---->croppedPath returned "+croppedPath);
-        this.commonUtils.presentLoader('saving...');
-        this.savePhoto(newFileName, croppedPath, sourceType).then (success=>{
-          if (!item) {
-            this.selectedCategory.record.addItem({ notes: '', photo: [newFileName] });
-            this.commonUtils.removerLoader();
-          }
-          else {
-            this.selectedCategory.record.addPhotoToItem(newFileName,item);
-            this.commonUtils.removerLoader();
-          }
-        }, 
-          error=>{
-            this.commonUtils.removerLoader();
-            this.commonUtils.presentToast('error adding image','error');
-            console.log (JSON.stringify(error));
-          });
+        else {
+          this.selectedCategory.record.addPhotoToItem(newFileName, item);
+        }
       },
-      (whatever) => {
-        console.log ("Ignoring crop");
-        this.commonUtils.presentLoader('saving...');
-        this.savePhoto(newFileName, imagePath, sourceType).then (success=>{
+        error => {
           this.commonUtils.removerLoader();
-          if (!item) {
-            this.selectedCategory.record.addItem({ notes: '', photo: [newFileName] });
-          }
-          else {
-            this.selectedCategory.record.addPhotoToItem(newFileName,item);
-          }
-        }, 
-          error=>{
-            this.commonUtils.removerLoader();
-            this.commonUtils.presentToast('error adding image','error');
-          });
-      }
-      );
-      //this.savePhoto(imagePath, sourceType);
-      
-      
-
-    }, (err) => {
-      this.commonUtils.presentToast('Error while selecting image.', 'error');
-    });
+          this.commonUtils.presentToast('error saving image', 'error');
+        });
+    },// getpicture
+    (error) => {console.log (JSON.stringify(error));this.commonUtils.presentToast('error taking image', 'error');});
   }
 
-  savePhoto(newFileName, imagePath,sourceType):Promise <any>  {
-  
-    console.log ("--->savePhoto newFileName="+newFileName+" imagePath="+imagePath);
-      
-     
-      if (0 && this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
-        this.filePath.resolveNativePath(imagePath)
-          .then(filePath => {
-            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+  savePhoto(newFileName, imagePath, sourceType): Promise<any> {
 
-            console.log ("Android: copying "+newFileName+" to "+correctPath+currentName);
-             console.log ("album calling copyFileToLocalDir with newFileName:"+newFileName+"  correctPath+currentName="+correctPath+currentName);
-            return this.copyFileToLocalDir(correctPath, currentName, newFileName);
-           
-          });
-      } else {
-        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-        console.log ("calling copyFileToLocalDir with newFileName:"+newFileName+"  correctPath+currentName="+correctPath+currentName);
-        return this.copyFileToLocalDir(correctPath, currentName, newFileName);
-       
-      }
+    console.log("--->savePhoto newFileName=" + newFileName + " imagePath=" + imagePath);
+
+
+    if (0 && this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+      this.filePath.resolveNativePath(imagePath)
+        .then(filePath => {
+          let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+          let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+
+          console.log("Android: copying " + newFileName + " to " + correctPath + currentName);
+          console.log("album calling copyFileToLocalDir with newFileName:" + newFileName + "  correctPath+currentName=" + correctPath + currentName);
+          return this.copyFileToLocalDir(correctPath, currentName, newFileName);
+
+        });
+    } else {
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      console.log("calling copyFileToLocalDir with newFileName:" + newFileName + "  correctPath+currentName=" + correctPath + currentName);
+      return this.copyFileToLocalDir(correctPath, currentName, newFileName);
+
+    }
 
   }
 
@@ -408,7 +369,7 @@ modal.present();*/
 
 
   ngAfterViewInit() {
-    
-}
+
+  }
 
 }
